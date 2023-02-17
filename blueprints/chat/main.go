23517d5b921988d9a16b9661214540ab6468bc6c
RenatoValentim <rvrenato28@gmail.com>
2023-02-17 00:40:03 -0300

    feat: building an HTML and JavaScript chat client

--- blueprints/chat/main.go
+++ blueprints/chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -20,10 +21,12 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			template.ParseFiles(filepath.Join("templates", t.fileName)),
 		)
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application")
+	flag.Parse()
 	r := newRoom()
 
 	http.Handle("/", &templateHandler{fileName: "chat.html"})
@@ -31,7 +34,8 @@ func main() {
 
 	go r.run()
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
