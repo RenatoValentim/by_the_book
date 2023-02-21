e06a73873d38968f0c374d1858083872e78b5ab0
RenatoValentim <rvrenato28@gmail.com>
2023-02-21 01:37:31 -0300

    refactor: implement final logic

--- entendendo_algoritmos/busca_binaria.py
+++ entendendo_algoritmos/busca_binaria.py
@@ -1,5 +1,16 @@
 from typing import Any
 
 
-def busca_binaria(lista: Any, valor: Any) -> int:
-    return 2
+def busca_binaria(lista: Any, item: Any) -> int | None:
+    baixo = 0
+    alto = len(lista) - 1
+    while baixo <= alto:
+        meio = int((baixo + alto) / 2)
+        chute = lista[meio]
+        if chute == item:
+            return meio
+        if chute > item:
+            alto = meio - 1
+        else:
+            baixo = meio + 1
+    return None
--- entendendo_algoritmos/busca_binaria_test.py
+++ entendendo_algoritmos/busca_binaria_test.py
@@ -5,6 +5,15 @@ def test_deve_retornar_o_indice_do_valor_caso_ele_exista_na_lista():
     numeros = [1, 2, 3, 4, 5]
     valor = 3
 
-    resultado = busca_binaria(lista=numeros, valor=valor)
+    resultado = busca_binaria(lista=numeros, item=valor)
 
     assert resultado == 2
+
+
+def test_deve_retornar_None_caso_ele_nao_exista_na_lista():
+    numeros = [1, 2, 3, 4, 5]
+    valor = 8
+
+    resultado = busca_binaria(lista=numeros, item=valor)
+
+    assert resultado is None
